4e81054e85eede7850aeb4cdea96525617411d18
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-07 18:02:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const giphyJson = require("./giphy.json");
 
 function App() {
+  const gj = giphyJson.data.map((giphy) => {
+    return <Giphy giphy={giphy}></Giphy>;
+  });
+  return <div>{gj}</div>;
+}
+
+//making compnent using prop CL
+
+
+//extra variable 'let user' om met een if conditie te checken of user bestaat
+const Giphy = (props) => {
+  let user
+  if(props.giphy.user){
+    user = (
+      <div>
+        <img
+          src={props.giphy.user.avatar_url}
+          alt="avatarpicture"
+        />
+        <span>{props.giphy.user.display_name}</span>
+      </div>
+    )
+  }
+//terniary embedded in code to check if user exists 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user}
+      {/* <div>
+        <img
+          src={props.giphy.user ? props.giphy.user.avatar_url : ""}
+          alt="avatarpicture"
+        />
+        <span>{props.giphy.user ? props.giphy.user.display_name : ""}</span>
+      </div> */}
+      <img src={props.giphy.images.original.url} alt={props.giphy.title} />
+      <video controls width={props.giphy.images.original.width}>
+        <source src={props.giphy.images.original.mp4} type="video/mp4"></source>
+      </video>
     </div>
   );
-}
+};
 
 export default App;
